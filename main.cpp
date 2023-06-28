560656879120c30049dec1fcd5b39bb9264af489
DaiCC-Aurora <daicc.aurora@gmail.com>
2023-06-28 13:49:58 +0800

    initialize

--- main.cpp
+++ main.cpp
@@ -3,14 +3,6 @@
 #include "Detector.h"
 
 int main(int argc, char *argv[]) {
-    // test
-    Detector det;
-    det.loadModule("C:/Users/Administrator/Desktop/20230413/status.onnx");
-    Mat img = imread("C:/001.jpg");
-    deb;
-    Mat f = det.Detect(img); deb;
-    imshow("Window", f);
-
     QApplication a(argc, argv);
     DetectorGui w;
     w.show();
